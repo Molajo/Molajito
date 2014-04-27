392c9dfd96add17b23637d8c9eef257e0f182d5f
AmyStephen <AmyStephen@gmail.com>
2014-04-27 05:39:53 -0500

    Data Gathering Process

--- .dev/Bootstrap.php
+++ .dev/Bootstrap.php
@@ -6,25 +6,30 @@
  * @copyright  2014 Amy Stephen. All rights reserved.
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
  */
-$base     = substr(__DIR__, 0, strlen(__DIR__) - 5);
-include $base . '/vendor/autoload.php';
-include_once __DIR__ . '/CreateClassMap.php';
+$base = substr(__DIR__, 0, strlen(__DIR__) - 5);
+if (function_exists('CreateClassMap')) {
+} else {
+    include_once __DIR__ . '/CreateClassMap.php';
+}
+include_once $base . '/vendor/autoload.php';
 
 $classmap = array();
-$results  = createClassMap($base . '/Source/Data/', 'Molajito\\Data\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Escape/', 'Molajito\\Escape\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Event/', 'Molajito\\Event\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Translate/', 'Molajito\\Translate\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/View/', 'Molajito\\View\\');
-$classmap = array_merge($classmap, $results);
+$classmap['Molajito\\Data\\Molajo'] = $base . '/Source/Data/Molajo.php';
+$classmap['Molajito\\Data\\AbstractAdapter'] = $base . '/Source/Data/AbstractAdapter.php';
+$classmap['Molajito\\Escape\\Molajo'] = $base . '/Source/Escape/Molajo.php';
+$classmap['Molajito\\Escape\\Simple'] = $base . '/Source/Escape/Simple.php';
+$classmap['Molajito\\Escape\\AbstractAdapter'] = $base . '/Source/Escape/AbstractAdapter.php';
+$classmap['Molajito\\Event\\Molajo'] = $base . '/Source/Event/Molajo.php';
+$classmap['Molajito\\Event\\Dummy'] = $base . '/Source/Event/Dummy.php';
+$classmap['Molajito\\Event\\AbstractAdapter'] = $base . '/Source/Event/AbstractAdapter.php';
+$classmap['Molajito\\Translate\\MolajoLanguageAdapter'] = $base . '/Source/Translate/MolajoLanguageAdapter.php';
+$classmap['Molajito\\Translate\\StringArrayAdapter'] = $base . '/Source/Translate/StringArrayAdapter.php';
+$classmap['Molajito\\Translate\\AbstractAdapter'] = $base . '/Source/Translate/AbstractAdapter.php';
+$classmap['Molajito\\View\\Molajo'] = $base . '/Source/View/Molajo.php';
+$classmap['Molajito\\View\\Filesystem'] = $base . '/Source/View/Filesystem.php';
+$classmap['Molajito\\View\\AbstractAdapter'] = $base . '/Source/View/AbstractAdapter.php';
 $results  = createClassMap($base . '/Source/', 'Molajito\\');
 $classmap = array_merge($classmap, $results);
-
-ksort($classmap);
 
 spl_autoload_register(
     function ($class) use ($classmap) {
